brandonyee333/test

LPS-195679 Upgrade Process

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -330,6 +330,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"7.1.1", "8.0.0",
 			new com.liferay.object.internal.upgrade.v8_0_0.
 				ObjectFolderItemUpgradeProcess(_portalUUID));
+
+		registry.register(
+			"8.0.0", "8.1.0",
+			new com.liferay.object.internal.upgrade.v8_1_0.
+				ObjectValidationRuleUpgradeProcess());
 	}
 
 	@Reference

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v8_1_0/ObjectValidationRuleUpgradeProcess.java
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v8_1_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+
+/**
+ * @author Pedro Leite
+ */
+public class ObjectValidationRuleUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("update ObjectValidationRule set system_ = [$FALSE$]");
+	}
+
+	@Override
+	protected UpgradeStep[] getPreUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.addColumns(
+				"ObjectValidationRule", "system_ BOOLEAN")
+		};
+	}
+
+}

JozGonzalez/liferay-portal

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -340,6 +340,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"8.1.0", "8.2.0",
 			new com.liferay.object.internal.upgrade.v8_2_0.
 				ObjectValidationRuleSettingsUpgradeProcess());
+
+		registry.register(
+			"8.2.0", "8.3.0",
+			new com.liferay.object.internal.upgrade.v8_3_0.
+				ObjectValidationRuleUpgradeProcess());
 	}
 
 	@Reference

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v8_3_0/ObjectValidationRuleUpgradeProcess.java
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v8_3_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+
+/**
+ * @author Pedro Leite
+ */
+public class ObjectValidationRuleUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("update ObjectValidationRule set system_ = [$FALSE$]");
+	}
+
+	@Override
+	protected UpgradeStep[] getPreUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.addColumns(
+				"ObjectValidationRule", "system_ BOOLEAN")
+		};
+	}
+
+}